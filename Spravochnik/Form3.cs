29c727c83e5cafebbd2feab3873af80b59c674b6
StuffyMan47 <fazleev_01@mail.ru>
2022-07-18 00:02:31 +0300

    При введении "кода сотрудника" в форму 5 ("Изменить") в текст боксы записываются данные из бд

--- Spravochnik/Form3.cs
+++ Spravochnik/Form3.cs
@@ -25,8 +25,7 @@ namespace Spravochnik
         private void button1_Click(object sender, EventArgs e)
         {
             int kod = Convert.ToInt32(textBox1.Text);
-            string query = "UPDATE table_name SET Фамилия = '" + textBox2.Text + "',Имя = '" + textBox3.Text + "',Отчество = '" + textBox4.Text + ",Должность = '" + textBox5.Text + "',ВН = '" + textBox6.Text + "',ГорН = '" + textBox7.Text + "' WHERE [Код сотрудника] = " + kod;
-            OleDbCommand command = new OleDbCommand(query, myConnection);
+            OleDbCommand command = new OleDbCommand("UPDATE table_name SET Фамилия = '" + textBox2.Text + "', Имя = '" + textBox3.Text + "', Отчество = '" + textBox4.Text + "', Должность = '" + textBox5.Text + "', ВН = '" + textBox6.Text + "', ГорН = '" + textBox7.Text + "' WHERE [Код сотрудника] = " + (Convert.ToInt32(textBox1.Text)), myConnection);
             command.ExecuteNonQuery();
             MessageBox.Show("Данные обновлены!", "Внимание!");
         }
@@ -54,6 +53,26 @@ namespace Spravochnik
             if (!Char.IsDigit(ch) && ch != 8)
             {
                 e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                OleDbCommand command = new OleDbCommand("SELECT * FROM table_name WHERE [Код сотрудника] = @ID", myConnection);
+                command.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+                command.ExecuteNonQuery();
+                OleDbDataReader oleDbDataReader = command.ExecuteReader();
+                while (oleDbDataReader.Read())
+                {
+                    textBox2.Text = oleDbDataReader.GetValue(1).ToString();
+                    textBox3.Text = oleDbDataReader.GetValue(2).ToString();
+                    textBox4.Text = oleDbDataReader.GetValue(3).ToString();
+                    textBox5.Text = oleDbDataReader.GetValue(4).ToString();
+                    textBox6.Text = oleDbDataReader.GetValue(5).ToString();
+                    textBox7.Text = oleDbDataReader.GetValue(6).ToString();
+                }
             }
         }
     }
